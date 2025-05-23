c70a233144fbb2de2704ad6dc3987e82713b963c
Guru Dev <guru01803@gmail.com>
2025-05-22 20:42:33 -0700

    Updated Day_14 SOLID practice task

---
+++ Day_14/SolidPrinciples/Models/Bird.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.Models
+{
+    public abstract class Bird
+    {
+        public string Name { get; set; }
+    }
+    public interface IFlyable
+    {
+        void Fly();
+    }
+    public class Sparrow : Bird, IFlyable
+    {
+        public void Fly() => Console.WriteLine($"{Name} is Flying");
+    }
+    public class Penguin : Bird
+    {
+        public void Swim() => Console.WriteLine($"{Name} is swimming.");
+    }
+}
