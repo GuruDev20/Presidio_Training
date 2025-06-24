4939e5c52aff3f98f578fa06973f903bd0f242bd
Guru Dev <guru01803@gmail.com>
2025-06-24 15:54:00 +0530

    updated overview page with sample ui

--- Customer_Spport_Chatbot/Customer_Support.Chatbot.UI/src/app/app.routes.ts
+++ Customer_Spport_Chatbot/Customer_Support.Chatbot.UI/src/app/app.routes.ts
@@ -4,9 +4,6 @@ import { AdminDashboard } from './features/admin/dashboard/dashboard';
 import { AgentDashboard } from './features/agent/dashboard/dashboard';
 import { Auth_Routes } from './features/auth/auth.route';
 import { UserLayout } from './layout/user-layout/user-layout.component';
-import { OverviewComponent } from './features/user/overview/overview.component';
-import { HistoryComponent } from './features/user/history/history.component';
-import { TicketsComponent } from './features/user/tickets/tickets.component';
 
 export const routes: Routes = [
 
@@ -21,8 +18,8 @@ export const routes: Routes = [
         children:[
             {path:'',redirectTo:'overview',pathMatch:'full'},
             {path:'overview',loadComponent: () => import('./features/user/overview/overview.component').then(m => m.OverviewComponent) },
-            { path:'history',loadComponent: () => import('./features/user/history/history.component').then(m => m.HistoryComponent) },
-            { path:'tickets',loadComponent: () => import('./features/user/tickets/tickets.component').then(m => m.TicketsComponent) }
+            {path:'tickets',loadComponent: () => import('./features/user/tickets/tickets.component').then(m => m.TicketsComponent) },
+            {path:'history',loadComponent: () => import('./features/user/history/history.component').then(m => m.HistoryComponent) }
         ]
     },
     {path:'admin/dashboard',component:AdminDashboard,canActivate:[AuthGurad]},
