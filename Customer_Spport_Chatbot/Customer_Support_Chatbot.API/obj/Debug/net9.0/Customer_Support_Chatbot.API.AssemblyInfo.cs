9ff330e4cdcfc20f20b8f3461b13fc685b1fcbdb
Guru Dev <guru01803@gmail.com>
2025-06-30 09:12:53 +0530

    Remove embedded git repo and add UI project properly

--- Customer_Spport_Chatbot/Customer_Support_Chatbot.API/obj/Debug/net9.0/Customer_Support_Chatbot.API.AssemblyInfo.cs
+++ Customer_Spport_Chatbot/Customer_Support_Chatbot.API/obj/Debug/net9.0/Customer_Support_Chatbot.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customer_Support_Chatbot.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02ca01df808a98bcd2f6851922f2cc676a7109f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3b467e34b776d5d60ef5ff2f70c7ffdb82e3e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customer_Support_Chatbot.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customer_Support_Chatbot.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
