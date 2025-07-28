b15f1eab4d07d0f392aebcd1b5d110be9063bf68
krishnan74 <divyakrishnan.r74@gmail.com>
2025-07-28 13:47:04 +0530

    refactor: Standardize formatting in auth and subscription models for improved readability

--- Customer_Spport_Chatbot/Customer_Support_Chatbot.UI/src/app/models/subscription.model.ts
+++ Customer_Spport_Chatbot/Customer_Support_Chatbot.UI/src/app/models/subscription.model.ts
@@ -1,20 +1,20 @@
-import { PaymentModel } from "./payment";
+import { PaymentModel } from './payment';
 
 export interface SubscriptionModel {
-    id: string;
-    payment: PaymentModel;
-    subscriptionPlan: SubscriptionPlanModel;
-    startDate: Date;
-    endDate: Date;
-    status: 'Active' | 'Inactive' | 'Cancelled' | 'Expired' | 'Pending';
+  id: string;
+  payment: PaymentModel;
+  subscriptionPlan: SubscriptionPlanModel;
+  startDate: Date;
+  endDate: Date;
+  status: 'Active' | 'Inactive' | 'Cancelled' | 'Expired' | 'Pending';
 }
 
 export interface SubscriptionPlanModel {
-    id: string;
-    name: string;
-    price: number;
-    description: string;
-    features?: string[];
-    durationInDays: number;
-    highlight?: boolean; 
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  features?: string[];
+  durationInDays: number;
+  highlight?: boolean;
 }
