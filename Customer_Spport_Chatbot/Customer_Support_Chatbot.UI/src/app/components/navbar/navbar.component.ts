9dc8899c9cebef51c3e53dcffeff9de0e47125a0
krishnan74 <divyakrishnan.r74@gmail.com>
2025-07-30 13:29:09 +0530

    fix: Update user profile picture handling and refactor ticket agent assignment logic

--- Customer_Spport_Chatbot/Customer_Support_Chatbot.UI/src/app/components/navbar/navbar.component.ts
+++ Customer_Spport_Chatbot/Customer_Support_Chatbot.UI/src/app/components/navbar/navbar.component.ts
@@ -1,75 +1,73 @@
-import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, OnInit, Output } from "@angular/core";
-import { LucideIconsModule } from "../../utils/lucide-icons.module";
-import { AuthService } from "../../services/auth.service";
-import { Router } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { LucideIconsModule } from '../../utils/lucide-icons.module';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { UserProfile } from '../../models/auth.model';
 
 @Component({
-    selector:'app-navbar',
-    standalone: true,
-    templateUrl: './navbar.component.html',
-    imports:[CommonModule,LucideIconsModule]
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  imports: [CommonModule, LucideIconsModule],
 })
+export class NavbarComponent implements OnInit {
+  isDark = false;
+  userProfile: UserProfile | null = null;
+  @Output() openSidebar = new EventEmitter<void>();
 
-export class NavbarComponent implements OnInit{
-    isDark= false;
-    userProfile: any;
-    @Output() openSidebar=new EventEmitter<void>();
-    
-    constructor(private authService:AuthService,private router:Router){}
+  constructor(private authService: AuthService, private router: Router) {}
 
-    ngOnInit(): void {
-        this.authService.getCurrentUser().subscribe({
-            next: (res) => {
-                this.userProfile = res.data;
-            },
-            error: (err) => {
-                console.error('Error fetching user profile:', err);
-            }
-        });
-        this.isDark = document.documentElement.classList.contains('dark');
-    }
-    toggleTheme() {
-        this.isDark = !this.isDark;
-        const html= document.documentElement;
-        html.classList.toggle('dark', this.isDark);
-    }
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (res) => {
+        this.userProfile = res.data;
+      },
+      error: (err) => {
+        console.error('Error fetching user profile:', err);
+      },
+    });
+    this.isDark = document.documentElement.classList.contains('dark');
+  }
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    const html = document.documentElement;
+    html.classList.toggle('dark', this.isDark);
+  }
 
-    logout(){
-        const currentDeviceId = localStorage.getItem('deviceId');
-        if (currentDeviceId) {
-            this.authService.logoutDevice(currentDeviceId).subscribe({
-                next: () => {
-                    this.authService.logout();
-                    this.router.navigate(['/auth/login']);
-                },
-                error: (err) => {
-                    console.error("Error logging out current device:", err);
-                    this.authService.logout();
-                    this.router.navigate(['/auth/login']);
-                }
-            });
-        } 
-        else {
-            this.authService.logout();
-            this.router.navigate(['/auth/login']);
-        }
+  logout() {
+    const currentDeviceId = localStorage.getItem('deviceId');
+    if (currentDeviceId) {
+      this.authService.logoutDevice(currentDeviceId).subscribe({
+        next: () => {
+          this.authService.logout();
+          this.router.navigate(['/auth/login']);
+        },
+        error: (err) => {
+          console.error('Error logging out current device:', err);
+          this.authService.logout();
+          this.router.navigate(['/auth/login']);
+        },
+      });
+    } else {
+      this.authService.logout();
+      this.router.navigate(['/auth/login']);
     }
+  }
 
-    goToProfile() {
-        const role = this.userProfile?.role;
-        let baseRoute = '/user/dashboard';
-        if (role === 'Agent') {
-            baseRoute = '/agent/dashboard';
-        } 
-        else if (role === 'Admin') {
-            baseRoute = '/admin/dashboard';
-        }
-        this.router.navigate([`${baseRoute}/profile`]);
+  goToProfile() {
+    const role = this.userProfile?.role;
+    let baseRoute = '/user/dashboard';
+    if (role === 'Agent') {
+      baseRoute = '/agent/dashboard';
+    } else if (role === 'Admin') {
+      baseRoute = '/admin/dashboard';
     }
+    this.router.navigate([`${baseRoute}/profile`]);
+  }
 
-    get profileInitial():string{
-        if(!this.userProfile?.fullName) return 'U';
-        return this.userProfile.fullName.charAt(0).toUpperCase();
-    }
+  get profileInitial(): string {
+    if (!this.userProfile?.fullName) return 'U';
+    return this.userProfile.fullName.charAt(0).toUpperCase();
+  }
 }
