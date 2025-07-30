ae834b4318c675a6ce742e9f3d3cc74baa385f88
krishnan74 <divyakrishnan.r74@gmail.com>
2025-07-30 14:07:52 +0530

    feat: Add pricing route and update user sidebar constants for navigation

--- Customer_Spport_Chatbot/Customer_Support_Chatbot.UI/src/app/constants/user-sidebar.constant.ts
+++ Customer_Spport_Chatbot/Customer_Support_Chatbot.UI/src/app/constants/user-sidebar.constant.ts
@@ -1,5 +1,10 @@
-export const User_Sidebar_Constants=[
-    {label:'Overview',path:'/user/dashboard/overview',icon:'layout-dashboard'},
-    {label:'Tickets',path:'/user/dashboard/tickets',icon:'ticket'},
-    {label:'History',path:'/user/dashboard/history',icon:'clock'},
-]
+export const User_Sidebar_Constants = [
+  {
+    label: 'Overview',
+    path: '/user/dashboard/overview',
+    icon: 'layout-dashboard',
+  },
+  { label: 'Tickets', path: '/user/dashboard/tickets', icon: 'ticket' },
+  { label: 'History', path: '/user/dashboard/history', icon: 'clock' },
+  { label: 'Pricing', path: '/user/dashboard/pricing', icon: 'indian-rupee' },
+];
--- Customer_Spport_Chatbot/Customer_Support_Chatbot.UI/src/app/utils/lucide-icons.module.ts
+++ Customer_Spport_Chatbot/Customer_Support_Chatbot.UI/src/app/utils/lucide-icons.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Activity, Bell, ChevronDown, ChevronLeft, ChevronRight, CircleDashed, Clock, Clock10, Edit, LayoutDashboard, Lock, LockOpen, LogInIcon, LucideAngularModule, Menu, MessageCircle, MoreVertical, Plus, Settings, TerminalSquare, Ticket, UserRoundPen, Users, X } from 'lucide-angular';
+import { Activity, Bell, ChevronDown, ChevronLeft, ChevronRight, CircleDashed, Clock, Clock10, Edit, LayoutDashboard, Lock, LockOpen, LogInIcon, LucideAngularModule, Menu, MessageCircle, MoreVertical, Plus, Settings, TerminalSquare, Ticket, UserRoundPen, Users, X, IndianRupee } from 'lucide-angular';
 import { Camera, User, LogIn, LogOut, Sun, Moon } from 'lucide-angular';
 
 @NgModule({
     imports: [
         LucideAngularModule.pick({
+            IndianRupee,
             Camera,
             User,
             LogIn,
